62e87c16685d520668a80e41f9fcb4febd5fce56
Raymond Feng <rfeng@Raymonds-MacBook-Pro.local>
2018-03-05 22:16:55 -0800

    feat: add koa http server

---
+++ packages/http-server-koa/src/http-server-koa.ts
@@ -0,0 +1,74 @@
+// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// Node module: @loopback/http-server-koa
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import * as http from 'http';
+import * as https from 'https';
+import * as debugModule from 'debug';
+const debug = debugModule('loopback:http:server:koa');
+
+import {
+  BaseHttpContext,
+  BaseHandleHttp,
+  HttpEndpointFactory,
+  HttpEndpoint,
+  HttpServerConfig,
+} from '@loopback/http-server';
+
+import * as Koa from 'koa';
+
+import {Request, Response} from 'koa';
+export {Request, Response} from 'koa';
+
+export {HttpEndpoint, HttpServerConfig} from '@loopback/http-server';
+export type HttpContext = BaseHttpContext<Request, Response>;
+export type HandleHttp = BaseHandleHttp<Request, Response>;
+
+export class koaHttpEndpointFactory
+  implements HttpEndpointFactory<Request, Response> {
+  create(config: HttpServerConfig, handleHttp: HandleHttp) {
+    // Create an koa representing the server endpoint
+    const app = new Koa();
+    app.use(async (koaCtx, next) => {
+      const httpCtx: HttpContext = {
+        req: koaCtx.req,
+        res: koaCtx.res,
+        request: koaCtx.request,
+        response: koaCtx.response,
+        next: next,
+      };
+      await handleHttp(httpCtx);
+      await next();
+    });
+
+    let server: http.Server | https.Server;
+    if (config.protocol === 'https') {
+      server = https.createServer(
+        config.httpsServerOptions || {},
+        app.callback,
+      );
+    } else {
+      // default to http
+      server = http.createServer(app.callback);
+    }
+
+    server.listen(config.port, config.host);
+
+    return new Promise<HttpEndpoint>((resolve, reject) => {
+      server.once('listening', () => {
+        const host = config.host || server.address().address;
+        const port = server.address().port;
+        config.port = port;
+        config.host = host;
+        const protocol = config.protocol || 'http';
+        const url = `${protocol}://${host}:${port}`;
+        debug('Server is ready at %s', url);
+        resolve({server, url, container: app});
+      });
+      server.once('error', reject);
+    });
+  }
+}
+
+export const ENDPOINT_FACTORY = new koaHttpEndpointFactory();
---
+++ packages/http-server-koa/test/integration/http-server-koa.test.ts
@@ -0,0 +1,69 @@
+// Copyright IBM Corp. 2017,2018. All Rights Reserved.
+// Node module: @loopback/rest
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {supertest} from '@loopback/testlab';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+import {ENDPOINT_FACTORY, HttpServerConfig, HandleHttp} from '../..';
+
+describe('http-server-core (integration)', () => {
+  const factory = ENDPOINT_FACTORY;
+
+  it('supports http protocol', async () => {
+    const endpoint = await givenEndpoint({port: 0}, async httpCtx => {
+      httpCtx.response.body = 'Hello';
+    });
+
+    supertest(endpoint.url)
+      .get('/')
+      .expect(200, 'Hello');
+
+    return new Promise<void>((resolve, reject) => {
+      // tslint:disable-next-line:no-any
+      endpoint.server.close((err: any) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  });
+
+  it('supports https protocol', async () => {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    const key = fs.readFileSync(
+      path.join(__dirname, '../../../test/integration/privatekey.pem'),
+    );
+    const cert = fs.readFileSync(
+      path.join(__dirname, '../../../test/integration/certificate.pem'),
+    );
+    const endpoint = await givenEndpoint(
+      {protocol: 'https', httpsServerOptions: {cert, key}, port: 0},
+      async httpCtx => {
+        httpCtx.response.body = 'Hello';
+      },
+    );
+
+    supertest(endpoint.url)
+      .get('/')
+      .expect(200, 'Hello');
+    delete process.env.NODE_TLS_REJECT_UNAUTHORIZED;
+
+    return new Promise<void>((resolve, reject) => {
+      // tslint:disable-next-line:no-any
+      endpoint.server.close((err: any) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+  });
+
+  async function givenEndpoint(
+    options: HttpServerConfig,
+    handleHttp: HandleHttp,
+  ) {
+    return await factory.create(options || {}, handleHttp);
+  }
+});
